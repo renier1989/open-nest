7b58c4a0bb862a21440a6ed4ff60154c152e8f5f
Renier Vargas <renier1989@gmail.com>
2024-06-09 20:17:53 -0500

    ajustes en el prompt del asistente de para el use-case de ProsConsDicusser,

--- src/gpt/use-cases/prosConsDiscusser.use-case.ts
+++ src/gpt/use-cases/prosConsDiscusser.use-case.ts
@@ -15,9 +15,12 @@ export const prosConsDiscusserUseCase = async (
         content: `
             El usuario te dara una pregunta de la cual tendras que hacer una comparacion indicando cuales son los PROS Y CONTRAS.
             Si la pregunta del usuario no se corresponde o no es valida para hacer una comparacion, tu le diras amablemente que no puedes hacer una comparacion de lo que esta pregunta.
-            La respuesat debe ser en forma de markdown.
+            La respuesta debe ser en forma de markdown.
+            La respuesta no puede quedar incompleta, por lo que debes ajustar a la respuesta a un maximo de 400 palabras
             Los PROS y CONTRAS deben estar en una lista.
-            
+            Las palabras Claves que se esten comparando colocalas en una etiqueta de H1 y en Strong.
+            los Pors y Contras colocalas en una etiqueta strong y crealos como lista ordenadas.
+            Solo salta directamente a la comparacion y No des una introduccion.
             `,
       },
       {
@@ -34,5 +37,5 @@ export const prosConsDiscusserUseCase = async (
   });
 
   console.log(completion);
-  return completion.choices[0];
+  return completion.choices[0].message;
 };
