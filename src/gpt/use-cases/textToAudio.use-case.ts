30bd1775bdb2e145716d0ce334029df43bf32bd3
Renier Vargas <renier1989@gmail.com>
2024-06-17 06:46:24 -0500

    Generando un Mp3 con OpenAI y  guardandolo en un directorio del proyecto

--- src/gpt/use-cases/textToAudio.use-case.ts
+++ src/gpt/use-cases/textToAudio.use-case.ts
@@ -1,21 +1,41 @@
-import OpenAI from "openai";
+import * as fs from 'fs';
+import OpenAI from 'openai';
+import * as path from 'path';
 
 interface Options {
-    prompt: string;
-    voice?: string;
+  prompt: string;
+  voice?: string;
 }
 
-export const textToAudioUseCase = async (openai: OpenAI, {prompt , voice}:Options) => {
+export const textToAudioUseCase = async (
+  openai: OpenAI,
+  { prompt, voice }: Options,
+) => {
+  const voices = {
+    nova: 'nova',
+    alloy: 'alloy',
+    echo: 'echo',
+    fable: 'fable',
+    onyx: 'onyx',
+    shimmer: 'shimmer',
+  };
+  const selectedVoice = voices[voice] ?? 'nova';
+  const folderPath = path.resolve(__dirname, '../../../generated/audios/'); // definicion del path para almacenar
+  const speechFile = path.resolve(`${folderPath}/${new Date().getTime()}.mp3`); // definicion del nombre del archivo NOTA: se puede generar el mismo nombre de archivo si 2 o mas usuairos suben al mismo
+  fs.mkdirSync(folderPath, { recursive: true }); // definicion de la creacion de los directorios
 
-    const voices = {
-        'nova' :'nova',
-        'alloy': 'alloy',
-    }
+  const mp3 = await openai.audio.speech.create({
+    model: 'tts-1',
+    voice: selectedVoice,
+    input: prompt
+  });
 
-    const selectedVoice = voices[voice] ?? 'nova'; 
+  const buffer = Buffer.from( await mp3.arrayBuffer() );
+  fs.writeFileSync(speechFile, buffer);
 
-    return {
-        prompt: prompt,
-        selectedVoice: selectedVoice
-    }
-}
+  return {
+    prompt: prompt,
+    selectedVoice: selectedVoice,
+    file : speechFile,
+  };
+};
