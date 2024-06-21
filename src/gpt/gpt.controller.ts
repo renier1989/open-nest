044288daaa9de002c1bcdbbc3005b7f0338f1ae9
Renier Vargas <renier1989@gmail.com>
2024-06-21 07:00:46 -0500

    Cargando archvio al server para el endpoint de audio-to-text

--- src/gpt/gpt.controller.ts
+++ src/gpt/gpt.controller.ts
@@ -1,7 +1,27 @@
-import { Body, Controller, Get, HttpStatus, Param, Post, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  FileTypeValidator,
+  Get,
+  HttpStatus,
+  MaxFileSizeValidator,
+  Param,
+  ParseFilePipe,
+  Post,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { GptService } from './gpt.service';
-import { OrthographyDto, ProsConsDiscusserDto, TextToAudioDto, TranslateDto } from './dtos';
+import {
+  OrthographyDto,
+  ProsConsDiscusserDto,
+  TextToAudioDto,
+  TranslateDto,
+} from './dtos';
 import type { Response } from 'express';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 // esto es parte de lo que esta en la ruta gpt
 @Controller('gpt')
@@ -9,55 +29,49 @@ export class GptController {
   constructor(private readonly gptService: GptService) {}
 
   @Post('orthography-check')
-  orthographyCheck(
-    @Body() orthographyDto:OrthographyDto
-  ){
+  orthographyCheck(@Body() orthographyDto: OrthographyDto) {
     // return orthographyDto;
     return this.gptService.orthographyCheck(orthographyDto);
   }
 
   @Post('pros-cons-discusser')
-  prosConsDiscusser(
-    @Body() prosConsDiscusser:ProsConsDiscusserDto
-  ){
+  prosConsDiscusser(@Body() prosConsDiscusser: ProsConsDiscusserDto) {
     return this.gptService.prosConsDiscusser(prosConsDiscusser);
   }
-  
+
   // de esta forma se construye la peticion para que se pueda hacer el steam de los datos que openai devuelve
   @Post('pros-cons-discusser-stream')
   async prosConsDiscusserStream(
-    @Body() prosConsDiscusser:ProsConsDiscusserDto,
-    @Res() res: Response
-  ){
-    const stream = await this.gptService.prosConsDiscusserStream(prosConsDiscusser);
+    @Body() prosConsDiscusser: ProsConsDiscusserDto,
+    @Res() res: Response,
+  ) {
+    const stream =
+      await this.gptService.prosConsDiscusserStream(prosConsDiscusser);
 
     res.setHeader('Content-Type', 'application/json');
     res.status(HttpStatus.OK);
 
-    for await(const chunk of stream){
+    for await (const chunk of stream) {
       const piece = chunk.choices[0].delta.content || '';
       console.log(piece);
       res.write(piece);
     }
 
     res.end();
-    
   }
 
   @Post('translate')
-  translate(
-    @Body() translate:TranslateDto
-  ){
+  translate(@Body() translate: TranslateDto) {
     return this.gptService.translate(translate);
   }
 
   @Post('text-to-audio')
   async textToAudio(
-    @Body() textToAudioDto:TextToAudioDto,
-    @Res() res:Response
-  ){
+    @Body() textToAudioDto: TextToAudioDto,
+    @Res() res: Response,
+  ) {
     const response = await this.gptService.textToAudio(textToAudioDto);
-    const {filePath}=response
+    const { filePath } = response;
     res.setHeader('Content-Type', 'audio/mp3');
     res.status(HttpStatus.OK);
     res.sendFile(filePath);
@@ -66,17 +80,42 @@ export class GptController {
   @Get('text-to-audio/:fileId')
   async textToAudioGetter(
     // @Body() textToAudioDto:TextToAudioDto,
-    @Res() res:Response,
-    @Param('fileId') fileId:string
-  ){
-
+    @Res() res: Response,
+    @Param('fileId') fileId: string,
+  ) {
     const filePath = await this.gptService.textToAudioGetter(fileId);
     res.setHeader('Content-Type', 'audio/mp3');
     res.status(HttpStatus.OK);
     res.sendFile(filePath);
   }
 
-
-
-
+  @Post('audio-to-text')
+  // este metodo es para subirlo
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './generated/uploads/',
+        filename: (req, file, callback) => {
+          const fileExtension = file.originalname.split('.').pop();
+          const fileName = `${new Date().getTime()}.${fileExtension}`;
+          return callback(null, fileName);
+        },
+      }),
+    }),
+  )
+  async audioToText(
+    // esto es para validar el archivo
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({maxSize:1000 * 1024 * 5, message:'Max file size is 5Mb.'}),
+          new FileTypeValidator({fileType: 'audio/*'})
+        ]
+      })
+    ) file : Express.Multer.File
+  ) {
+    console.log({file});
+    
+    return 'done';
+  }
 }
