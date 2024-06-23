44b9eeed2bc2de9f1d58ff887a782d6610eaf671
Renier Vargas <renier1989@gmail.com>
2024-06-23 14:36:41 -0500

    adelantanto el servicio para la edicion de las imagenes

--- src/gpt/use-cases/imageGeneration.use-case.ts
+++ src/gpt/use-cases/imageGeneration.use-case.ts
@@ -1,6 +1,7 @@
 import OpenAI from "openai";
-import { downloadImageAsPng } from "src/helpers";
-
+import { downloadBase64ImageAsPng, downloadImageAsPng } from "src/helpers";
+import * as fs from "fs";
+import * as path from "path";
 interface Options {
     prompt: string;
     originalImage?: string;
@@ -11,22 +12,49 @@ export const imageGenerationUseCase = async ( openai: OpenAI , options: Options)
 
     const {prompt,originalImage,maskImage} =  options;
     // TODO: VERIFICAR EL ORIGINAL IMAGE
+    if(!originalImage || !maskImage){
+        const response = await openai.images.generate({
+            prompt: prompt,
+            model: 'dall-e-3',
+            n:1,
+            quality:'standard',
+            size: '1024x1024',
+            // style:'natural',
+            response_format: 'url'
+        });
+        // TODO: TOMAR LA IMAGEN Y GUARDARLA EN EL FS
+        const url = await downloadImageAsPng(response.data[0].url);
+    
+        return {
+            url : url , // TODO: localhost:3000/gpt/image-generation/1719167207446.png  - el endpoint para obtener la imagen
+            openaiUrl : response.data[0].url,
+            revised_prompt:  response.data[0].revised_prompt
+        }
+    }
 
-    const response = await openai.images.generate({
-        prompt: prompt,
-        model: 'dall-e-3',
-        n:1,
-        quality:'standard',
+    // esto es para la edicion de la imagen
+    // originalImage = localhost:3000/gpt/image-generation/1719167207446.png  //ejemplo
+    // maskImagePath = base64;fñlakjfñlkhsdñlfkjgadfksfjk45j6ñ345k3n45kj // ejemplo
+    const pngImagePath = await downloadImageAsPng(originalImage);
+    const maskImagePath = await downloadBase64ImageAsPng(maskImage);
+
+    const response = await openai.images.edit({
+        model: 'dall-e-2',
+        prompt:prompt,
+        image: fs.createReadStream(pngImagePath),
+        mask: fs.createReadStream(maskImagePath),
+        n: 1,
         size: '1024x1024',
-        // style:'natural',
         response_format: 'url'
-    });
-    // TODO: TOMAR LA IMAGEN Y GUARDARLA EN EL FS}
+    })
 
-    const url = await downloadImageAsPng(response.data[0].url);
+    const localImagePath = await downloadImageAsPng(response.data[0].url);
+    const fileName = path.basename(localImagePath);
+
+    const publicUrl = `localhost:3000/${fileName}`; // temporal no es esto
 
     return {
-        url : url,
+        url : publicUrl , // TODO: localhost:3000/gpt/image-generation/1719167207446.png  - el endpoint para obtener la imagen
         openaiUrl : response.data[0].url,
         revised_prompt:  response.data[0].revised_prompt
     }
--- src/helpers/download-image-as-png.ts
+++ src/helpers/download-image-as-png.ts
@@ -1,26 +1,43 @@
-import * as path from "path";
+import * as path from 'path';
 import * as fs from 'fs';
 import * as sharp from 'sharp';
-import { InternalServerErrorException } from "@nestjs/common";
+import { InternalServerErrorException } from '@nestjs/common';
 
-export const downloadImageAsPng = async(url:string) =>{
+export const downloadImageAsPng = async (url: string) => {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new InternalServerErrorException('Downloading image failed.');
+  }
 
-    const response = await fetch(url);
-    if(!response.ok){
-        throw new InternalServerErrorException('Downloading image failed.');
-    }
+  const folderPath = path.resolve('./', './generated/images/');
+  fs.mkdirSync(folderPath, { recursive: true }); // con esto verificamos el directorio y si no existe lo crea
 
-    const folderPath = path.resolve('./','./generated/images/');
-    fs.mkdirSync(folderPath, { recursive: true}); // con esto verificamos el directorio y si no existe lo crea
+  const imageNamePng = `${new Date().getTime()}.png`;
+  const buffer = Buffer.from(await response.arrayBuffer());
 
-    const imageNamePng = `${ new Date().getTime()}.png`;
-    const buffer = Buffer.from( await response.arrayBuffer());
+  // fs.writeFileSync(`${ folderPath }/${ imageName }`, buffer); // esta la configuracion para guardar la imagen con el formato de venga
 
-    // fs.writeFileSync(`${ folderPath }/${ imageName }`, buffer); // esta la configuracion para guardar la imagen con el formato de venga
+  const completePath = path.join(folderPath, imageNamePng);
+  await sharp(buffer).png().ensureAlpha().toFile(completePath); // con esto guardamos la imagen siempre en forma PNG
+  return completePath;
+};
 
-    const completePath = path.join(folderPath, imageNamePng);
-    await sharp( buffer).png().ensureAlpha().toFile(completePath); // con esto guardamos la imagen siempre en forma PNG
-    return completePath;
+export const downloadBase64ImageAsPng = async (base64Image: string) => {
+  // Remover encabezado
+  base64Image = base64Image.split(';base64,').pop();
+  const imageBuffer = Buffer.from(base64Image, 'base64');
 
+  const folderPath = path.resolve('./', './generated/images/');
+  fs.mkdirSync(folderPath, { recursive: true });
 
-}
+  const imageNamePng = `${new Date().getTime()}-64.png`;
+
+  // Transformar a RGBA, png // Así lo espera OpenAI
+  const completePath = path.join(folderPath, imageNamePng);
+  await sharp(imageBuffer)
+    .png()
+    .ensureAlpha()
+    .toFile(completePath);
+
+  return completePath;
+};
